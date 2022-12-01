c808ca69f569f281e73e02ec18bb907b242f4a9f
Rob R <r0brh0d3s@gmail.com>
2022-12-01 10:11:03 -0800

    more dev again

--- src/App.vue
+++ src/App.vue
@@ -3,54 +3,61 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <h1>Starsixty9</h1>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/artists">Artists</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
-
-  <RouterView />
+  <div class="center-me">
+    <header>
+      <h1>Starsixty9</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/artists">Artists</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
+  
+    <RouterView />
+  </div>
 </template>
 
 <style lang="scss" scoped>
-header {
-  display: flex;
-  align-items: center;
-}
-
-nav {
-  font-size: 12px;
-  text-align: center;
-  a {
-    color: #2786ce;
-    font-size: 1.5em;
-    text-decoration: none;
-
-    &:hover {
-      color: #cb6161;
+.center-me {
+  max-width: 500px;
+  margin: auto;
+  header {
+    display: flex;
+    align-items: center;
+  }
+  
+  nav {
+    font-size: 12px;
+    text-align: center;
+    a {
+      color: #2786ce;
+      font-size: 1.5em;
+      text-decoration: none;
+  
+      &:hover {
+        color: #cb6161;
+      }
     }
+  }
+  
+  nav a.router-link-exact-active {
+    color: var(--color-text);
+  }
+  
+  nav a.router-link-exact-active:hover {
+    background-color: transparent;
+  }
+  
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+  }
+  
+  nav a:first-of-type {
+    border: 0;
   }
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 
 </style>
